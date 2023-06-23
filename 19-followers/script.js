dbcfa41a9f4032a590a131170ade93355984dba2
Daniel Baez <68305041+D-baez96@users.noreply.github.com>
2023-06-22 21:40:09 -0500

    Add files via upload

---
+++ 19-followers/script.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(item =>{
+    item.innerText = "0"
+    const target =  + item.getAttribute('data-target');
+    const interval = target/100
+
+    const updateCounter = () =>{
+        const value = +item.innerText;
+        if (value < target ){
+            item.innerText = Math.ceil(value + interval);
+            setTimeout(() =>{
+                updateCounter();
+            },40);
+        }
+    };
+    updateCounter();
+})
